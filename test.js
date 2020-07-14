4075288babc160a2b3bbbc9602a329030028780c
Dagur Leó Bergsson <dagurleo@gmail.com>
2020-07-14 16:14:40 +0900

    Test github actions

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ const DateTimer = require('./index')
 // console.log(Timer)
 const test = new DateTimer().subYears(131).subMonths(2).subDays(12)
 console.log(test)
-test.
+
 // console.log(formatter.format())
